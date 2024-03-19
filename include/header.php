bdc0732485dfcf69a80ed8e141384cb321a0f111
odivibe <elvisway4u@gmail.com>
2024-03-19 16:50:12 +0100

    css update

--- include/header.php
+++ include/header.php
@@ -1,46 +1,65 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Fashion Ecommerce</title>
-<link rel="stylesheet" href="css/main.css">
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fashion Ecommerce</title>
+    <link rel="stylesheet" href="css/main.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />-->
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
 </head>
 <body>
 
-<div class="mobile-header">
-    <div class="menu-icon">
-        <a href="#" class="action menu"><i class="material-icons">menu</i></a>
+<header>
+    <div class="mobile-header">
+        <div class="menu-icon header-children">
+            <a href="#" class="action menu"><i class="material-icons">menu</i></a>
+        </div>
+        <div class="category header-children">
+            <ul>
+                <li><a href="#">Menswear</a></li>
+                <li><a href="#">Womenswear</a></li>
+                <li><a href="#">Kidswear</a></li>
+            </ul>
+        </div>
+        <div class="logo header-children">
+            <img src="logo.png" alt="Company Logo">
+            <h1>Company Name</h1>
+        </div>
+        <div class="icons header-children">
+            <a href="#" class="action search"><i class="material-icons">search</i></a>
+            <a href="#" class="action whishlist"><i class="material-icons">favorite</i></a>
+            <a href="#" class="action cart"><i class="material-icons">shopping_cart</i></a>
+        </div>
     </div>
-    <div class="logo">
-        <img src="logo.png" alt="Company Logo">
-        <h1>Company Name</h1>
+
+    <nav class="desktop-navigation">
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Shop</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+
+    <nav class="mobile-navigation">
+        <ul>
+            <li><a href="#">mobile 1</a></li>
+            <li><a href="#">mobile 2</a></li>
+            <li><a href="#">mobile 3</a></li>
+            <li><a href="#">mobile 4</a></li>
+        </ul>
+        <a href="#" class="close-menu"><i class="material-icons">close</i></a>
+    </nav>
+
+    <div class="search-form-mobile">
+        <a href="#" class="close-search"><i class="material-icons">close</i></a>
+        <form action="#" method="GET">
+            <input type="text" name="search" placeholder="Search...">
+            <button type="submit">Search</button>
+        </form>
     </div>
-    <div class="icons">
-        <a href="#" class="action search"><i class="material-icons">search</i></a>
-        <a href="#" class="action cart"><i class="material-icons">shopping_cart</i></a>
-    </div>
-</div>
-
-<nav class="mobile-navigation">
-    <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Shop</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Contact</a></li>
-    </ul>
-    <a href="#" class="close-menu"><i class="material-icons">close</i></a>
-</nav>
-
-<div class="search-form-mobile">
-    <a href="#" class="close-search"><i class="material-icons">close</i></a>
-    <form action="#" method="GET">
-        <input type="text" name="search" placeholder="Search...">
-        <button type="submit">Search</button>
-    </form>
-</div>
-
+</header>
 
 
